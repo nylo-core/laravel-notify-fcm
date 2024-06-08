7c81168f79c6e6021ac60dad85a5dcf3d814beea
Anthony <ants52@aol.com>
2024-06-08 04:04:39 +0100

    v0.0.3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:laravel_notify_fcm/laravel_notify_fcm.dart';
--- test/laravel_notify_fcm_test.dart
+++ test/laravel_notify_fcm_test.dart
@@ -1,7 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:laravel_notify_fcm/laravel_notify_fcm.dart';
-
 void main() {
-  test('adds one to input values', () {});
+
 }
